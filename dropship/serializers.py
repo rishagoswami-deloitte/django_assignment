731c4384796825e5a16507e873ac188db56f3cca
Goswami_ Rishabh (rishagoswami@deloitte.com) <rishagoswami@your.hostname.com>
2022-08-08 23:35:44 +0530

    Few changes in third milestone

--- dropship/serializers.py
+++ dropship/serializers.py
@@ -13,7 +13,32 @@ class StandardUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.StandardUser
-        fields = "__all__"
+        fields = ('id', 'f_name', 'l_name', 'email', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create_user(self, validated_data):
+        user = models.User.objects.create_user(
+            validated_data['email'], validated_data['email'], validated_data['password'])
+        obj = models.StandardUser.objects.create(
+            first_name=validated_data['f_name'],
+            last_name=validated_data['l_name'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            user=user
+        )
+        return obj
+
+    def update_user(self, instance, validated_data):
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+        obj = models.StandardUser.objects.get(id=instance.id)
+        return super().update(obj, validated_data)
 
 
 class IssueSerializer(serializers.ModelSerializer):
